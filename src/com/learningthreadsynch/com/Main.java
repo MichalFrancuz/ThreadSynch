655a0145e1a87163342f3bd987eb3faca53a939a
Michał Francuz <104276165+michal9696@users.noreply.github.com>
2022-12-26 17:16:52 +0100

    Learning of Thread Synchronization

---
+++ src/com/learningthreadsynch/com/Main.java
@@ -0,0 +1,41 @@
+package com.learningthreadsynch.com;
+
+import javax.sql.rowset.FilteredRowSet;
+
+public class Main {
+    public static void main(String[] args) {
+        First first = new First();
+        Second one = new Second(first, "welcome");
+        Second two = new Second(first, "new");
+        Second three = new Second(first, "programmer");
+
+
+    }
+}
+
+class First {
+    synchronized void display(String msg) {
+        System.out.print(" [ " + msg);
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(" ] ");
+    }
+
+}
+
+class Second extends Thread {
+    String msg;
+    First fobj;
+    Second (First fb, String str) {
+        fobj = fb;
+        msg = str;
+        start();
+    }
+    public void run() {
+        fobj.display(msg);
+    }
+
+}
